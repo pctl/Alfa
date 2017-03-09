4ee1719db0862dddaedf170d2a453af576f82c22
Georgia <quatregeorgia@gmail.com>
2017-03-09 19:37:13 +0300

    hj

--- app/src/main/java/com/example/georgia/alfa/Activities/MainActivity.java
+++ app/src/main/java/com/example/georgia/alfa/Activities/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.georgia.alfa.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -24,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
        // Intent TerritoryIntent=new Intent(getApplicationContext(),)
         Toast.makeText(getApplicationContext(),"you are login in",Toast.LENGTH_LONG).show();
 
-        Intent myIntent=new Intent(getApplicationContext(),Territories.class);
-        startActivity(myIntent);
+        //Intent myIntent=new Intent(getApplicationContext(),Territories.class);
+       // startActivity(myIntent);
     }
 }
